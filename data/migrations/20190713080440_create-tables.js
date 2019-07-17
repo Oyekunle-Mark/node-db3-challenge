24f91537ffe3e501886478695cc89b68a001dae3
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-17 13:51:50 +0100

    Created the get steps helper method

--- data/migrations/20190713080440_create-tables.js
+++ data/migrations/20190713080440_create-tables.js
@@ -1,20 +1,21 @@
-
-exports.up = function(knex) {
-  return knex.schema
+exports.up = knex =>
+  knex.schema
     .createTable('schemes', tbl => {
       tbl.increments();
-      tbl.text('scheme_name', 128)
+      tbl
+        .text('scheme_name', 128)
         .unique()
         .notNullable();
     })
     .createTable('steps', tbl => {
       tbl.increments();
-      tbl.integer('step_number')
+      tbl
+        .integer('step_number')
         .unsigned()
         .notNullable();
-      tbl.text('instructions')
-        .notNullable();
-      tbl.integer('scheme_id')
+      tbl.text('instructions').notNullable();
+      tbl
+        .integer('scheme_id')
         .unsigned()
         .notNullable()
         .references('id')
@@ -22,10 +23,6 @@ exports.up = function(knex) {
         .onUpdate('CASCADE')
         .onDelete('CASCADE');
     });
-};
 
-exports.down = function(knex) {
-  return knex.schema
-    .dropTableIfExists('steps')
-    .dropTableIfExists('schemes');
-};
+exports.down = knex =>
+  knex.schema.dropTableIfExists('steps').dropTableIfExists('schemes');
--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -15,7 +15,15 @@ const findById = async id => {
   return null;
 };
 
+const findSteps = id =>
+  db('steps as s')
+    .join('schemes as sc', 'sc.id', 's.scheme_id')
+    .select('s.id', 'sc.scheme_name', 's.step_number', 's.instructions')
+    .where({ ['sc.id']: id })
+    .orderBy('s.step_number', 'asc');
+
 module.exports = {
   find,
   findById,
+  findSteps,
 };
