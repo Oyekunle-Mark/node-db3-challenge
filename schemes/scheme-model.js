a842d071e52e1a945b1594a62680775ce20a1894
oyekunle-mark <oyekunlemac@gmail.com>
2019-07-17 13:22:42 +0100

    Created the findById helper method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -5,6 +5,17 @@ const db = knex(development);
 
 const find = () => db('schemes');
 
+const findById = async id => {
+  const scheme = await db('schemes')
+    .where({ id })
+    .first();
+
+  if (scheme) return scheme;
+
+  return null;
+};
+
 module.exports = {
   find,
+  findById,
 };
